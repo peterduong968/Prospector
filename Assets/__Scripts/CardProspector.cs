f69f248f58e988131e20d42fa8a8f09164e0cc98
peterduong968 <54965701+peterduong968@users.noreply.github.com>
2019-11-14 21:59:13 -0500

    Step 4. Converted from card to cardprospectors

---
+++ Assets/__Scripts/CardProspector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum eCardState { drawpile, tableau, target, discard }
+
+public class CardProspector : MonoBehaviour
+{
+    [Header("Set Dynamically: CardProspector")]
+    public eCardState state = eCardState.drawpile;
+    public List<CardProspector> hiddenBy = new List<CardProspector>();
+    public int layoutID;
+    public SlotDef slotDef;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -7,35 +7,50 @@ using System.Collections.Generic;
 
 public class Prospector : MonoBehaviour {
 
-	static public Prospector 	S;
+    static public Prospector S;
 
-	[Header("Set in Inspector")]
-	public TextAsset			deckXML;
-    public TextAsset            layoutXML;
+    [Header("Set in Inspector")]
+    public TextAsset deckXML;
+    public TextAsset layoutXML;
 
 
-	[Header("Set Dynamically")]
-	public Deck					deck;
-    public Layout               layout;
+    [Header("Set Dynamically")]
+    public Deck deck;
+    public Layout layout;
+    public List<CardProspector> drawPile;
 
-	void Awake(){
-		S = this;
-	}
+    void Awake() {
+        S = this;
+    }
 
-	void Start() {
-		deck = GetComponent<Deck> ();
-		deck.InitDeck (deckXML.text);
+    void Start() {
+        deck = GetComponent<Deck>();
+        deck.InitDeck(deckXML.text);
         Deck.Shuffle(ref deck.cards);
 
-    /*    Card c;
-        for (int cNum=0; cNum<deck.cards.Count; cNum++)
-        {
-            c = deck.cards[cNum];
-            c.transform.localPosition = new Vector3((cNum % 13) * 13, cNum/13*4,0);
-        }
-    */
+        /*    Card c;
+            for (int cNum=0; cNum<deck.cards.Count; cNum++)
+            {
+                c = deck.cards[cNum];
+                c.transform.localPosition = new Vector3((cNum % 13) * 13, cNum/13*4,0);
+            }
+        */
         layout = GetComponent<Layout>();
         layout.ReadLayout(layoutXML.text);
-	}
 
-}
+        drawPile = ConvertListCardsToListCardProspectors(deck.cards);
+    }
+
+    List<CardProspector> ConvertListCardsToListCardProspectors(List<Card> lCD)
+    {
+        List<CardProspector> lCP = new List<CardProspector>();
+        CardProspector tCP;
+        foreach (Card tCD in lCD)
+        {
+            tCP = tCD as CardProspector;
+            lCP.Add(tCP);
+        }
+        return (lCP);
+    }
+
+    }
