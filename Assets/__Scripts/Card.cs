f9cc0d7270d5813a385096b969a679b1235a1e65
peterduong968 <54965701+peterduong968@users.noreply.github.com>
2019-11-15 15:22:47 -0500

    Step 6. Cards Stack Properly

--- Assets/__Scripts/Card.cs
+++ Assets/__Scripts/Card.cs
@@ -4,19 +4,65 @@ using System.Collections.Generic;
 
 public class Card : MonoBehaviour {
 
-	public string    suit;
-	public int       rank;
-	public Color     color = Color.black;
-	public string    colS = "Black";  // or "Red"
-	
-	public List<GameObject> decoGOs = new List<GameObject>();
-	public List<GameObject> pipGOs = new List<GameObject>();
-	
-	public GameObject back;  // back of card;
-	public CardDefinition def;  // from DeckXML.xml		
+    public string suit;
+    public int rank;
+    public Color color = Color.black;
+    public string colS = "Black";  // or "Red"
 
+    public List<GameObject> decoGOs = new List<GameObject>();
+    public List<GameObject> pipGOs = new List<GameObject>();
 
-	public bool faceUp {
+    public GameObject back;  // back of card;
+    public CardDefinition def;  // from DeckXML.xml		
+
+    public SpriteRenderer[] spriteRenderers;
+    void Start()
+    {
+        SetSortOrder(0);
+    }
+
+    public void PopulateSpriteRenderers()
+    {
+        if (spriteRenderers == null || spriteRenderers.Length == 0)
+        {
+            spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+        }
+    }
+
+    public void SetSortingLayerName(string tSLN)
+    {
+        PopulateSpriteRenderers();
+        foreach (SpriteRenderer tSR in spriteRenderers)
+        {
+            tSR.sortingLayerName = tSLN;
+        }
+    }
+
+    public void SetSortOrder(int sORD)
+    {
+        PopulateSpriteRenderers();
+        foreach (SpriteRenderer tSR in spriteRenderers)
+        {
+            if (tSR.gameObject == this.gameObject)
+            {
+                tSR.sortingOrder = sORD;
+                continue;
+            }
+            switch (tSR.gameObject.name)
+            {
+                case "back":
+                    tSR.sortingOrder = sORD + 2;
+                    break;
+
+                case "face":
+                default:
+                    tSR.sortingOrder = sORD + 1;
+                    break;
+            }
+        }
+    }
+
+    public bool faceUp {
 		get {
 			return (!back.activeSelf);
 		}
@@ -27,15 +73,6 @@ public class Card : MonoBehaviour {
 	}
 
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 } // class Card
 
 [System.Serializable]
