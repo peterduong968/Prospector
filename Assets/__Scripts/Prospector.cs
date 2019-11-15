55a25b88b8cf7294c57afb7fa0b220c29e30c929
peterduong968 <54965701+peterduong968@users.noreply.github.com>
2019-11-14 21:15:06 -0500

    Step 3. Can Parse from XML

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -27,12 +27,15 @@ public class Prospector : MonoBehaviour {
 		deck.InitDeck (deckXML.text);
         Deck.Shuffle(ref deck.cards);
 
-        Card c;
+    /*    Card c;
         for (int cNum=0; cNum<deck.cards.Count; cNum++)
         {
             c = deck.cards[cNum];
             c.transform.localPosition = new Vector3((cNum % 13) * 13, cNum/13*4,0);
         }
+    */
+        layout = GetComponent<Layout>();
+        layout.ReadLayout(layoutXML.text);
 	}
 
 }
